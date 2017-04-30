2e4ceee07ed4bf2d1fc732aaab46a5515356a526
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-01 01:54:40 +0700

    add japanese language

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "IFTTTish", lang: "eng" });
 });
+
+router.get('/jp', function(req, res, next) {
+  res.render('index', { title: "IFTTTish", lang: 'jp' });
+});
+
 
 module.exports = router;
